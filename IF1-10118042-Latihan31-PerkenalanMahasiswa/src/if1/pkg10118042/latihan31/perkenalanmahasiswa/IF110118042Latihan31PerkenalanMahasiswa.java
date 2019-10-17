12a0585e9392aaa0e1e5d9214c8fa3713abc177c
DhimasAjiG <55832635+DhimasAjiG@users.noreply.github.com>
2019-10-17 13:15:40 +0700

    Add files via upload

---
+++ IF1-10118042-Latihan31-PerkenalanMahasiswa/src/if1/pkg10118042/latihan31/perkenalanmahasiswa/IF110118042Latihan31PerkenalanMahasiswa.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10118042.latihan31.perkenalanmahasiswa;
+
+/**
+ * @author
+ * Nama  : Dhimas Aji Ghairrahmat
+ * Kelas : IF1
+ * NIM   : 10118042
+ * Deskripsi Program : Program ini menampilkan Nim dan Nama mahasiswa
+ * 
+ */
+public class IF110118042Latihan31PerkenalanMahasiswa {
+
+    public static void main(String[] args) {
+            Mahasiswa mhs = new Mahasiswa();
+           
+            
+            //output
+            mhs. nim = "10118042";
+            mhs.nama ="Dhimas Aji G";
+            mhs.perkenalanDiri();
+    }
+    
+}
---
+++ IF1-10118042-Latihan31-PerkenalanMahasiswa/src/if1/pkg10118042/latihan31/perkenalanmahasiswa/Mahasiswa.java
@@ -0,0 +1,17 @@
+
+package if1.pkg10118042.latihan31.perkenalanmahasiswa;
+
+public class Mahasiswa {
+public String nim;
+	public String nama;
+
+	public void perkenalanDiri() {
+	      
+                          System.out.println("Hallo Everyone");
+                          System.out.println("My NIM is " + nim);
+                          System.out.println("My name is " + nama);
+                          System.out.println();
+	}
+
+}    
+
